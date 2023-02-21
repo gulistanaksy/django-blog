71d4a7a90565dafdf860bc35432cdb99fd4e28e2
Gülistan Aksoy <gulistanaksoy87@gmail.com>
2023-02-21 16:42:15 +0300

    yazı admin paneli eklendi

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from blog.models import KategoriModel
+from blog.models import KategoriModel,YazilarModel
 
 
 admin.site.register(KategoriModel)
 
+
+class YazilarAdmin(admin.ModelAdmin):
+    search_fields = ('baslik','icerik')
+    list_display = (
+        'baslik','olusturulma_tarihi','duzenlenme_tarihi'
+    )
+
+admin.site.register(YazilarModel,YazilarAdmin)
