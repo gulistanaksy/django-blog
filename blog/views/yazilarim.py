0b24896d1864a16a63c03d161c59ea3643c303dc
Gülistan Aksoy <gulistanaksoy87@gmail.com>
2023-03-05 18:31:34 +0300

    yazi guncelleme ve silme islevi eklendi

--- blog/views/yazilarim.py
+++ blog/views/yazilarim.py
@@ -8,7 +8,7 @@ def yazilarim(request):
 
     yazilar = request.user.yazilar.order_by('-id')
     sayfa = request.GET.get('sayfa')
-    paginator = Paginator(yazilar,2)
+    paginator = Paginator(yazilar,5)
     return render(request, 'pages/yazilarim.html', context={
         'yazilar': paginator.get_page(sayfa)
     })
